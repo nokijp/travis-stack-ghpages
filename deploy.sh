92d0cd32c46d498913bd4235b5bc1477880af5d5
nokijp <nokijp@users.noreply.github.com>
2016-10-02 01:26:36 +0900

    add generate_index

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-GIT_USER='Travis CI'
-GIT_EMAIL="$DEPLOY_USER_EMAIL"
-GIT_COMMIT_MESSAGE='deploy to gh-pages'
+git_user='Travis CI'
+git_email="$DEPLOY_USER_EMAIL"
+git_commit_message='deploy to gh-pages'
 
-STACK_DIST_DIR="$(pwd)/$(stack path --dist-dir)"
-DEPLOY_KEY_ENC="$(pwd)/deploy_key.enc"
+stack_dist_dir="$(pwd)/$(stack path --dist-dir)"
+deploy_key_enc="$(pwd)/deploy_key.enc"
 
-TARGET_BRANCH=gh-pages
+target_branch=gh-pages
 
-GIT_REPO="$(git config remote.origin.url)"
-GIT_SSH_REPO="${GIT_REPO/https:\/\/github.com\//git@github.com:}"
+git_repo="$(git config remote.origin.url)"
+git_ssh_repo="${git_repo/https:\/\/github.com\//git@github.com:}"
 
 function setup_git() {
-  git config user.name "$GIT_USER"
-  git config user.email "$GIT_EMAIL"
+  git config user.name "$git_user"
+  git config user.email "$git_email"
 }
 
 function setup_ssh() {
@@ -24,7 +24,7 @@ function setup_ssh() {
   local encrypted_key_var="encrypted_${encryption_label}_key"
   local encrypted_iv_var="encrypted_${encryption_label}_iv"
 
-  openssl aes-256-cbc -K "${!encrypted_key_var}" -iv "${!encrypted_iv_var}" -in "$DEPLOY_KEY_ENC" -out deploy_key -d
+  openssl aes-256-cbc -K "${!encrypted_key_var}" -iv "${!encrypted_iv_var}" -in "$deploy_key_enc" -out deploy_key -d
   chmod 600 deploy_key
   eval "$(ssh-agent -s)"
   ssh-add deploy_key
@@ -34,30 +34,56 @@ function setup_dir() {
   local tmp_dir=dist
 
   rm -rf "$tmp_dir"
-  git clone "$GIT_REPO" "$tmp_dir"
+  git clone "$git_repo" "$tmp_dir"
   cd "$tmp_dir"
-  git checkout "$TARGET_BRANCH" || git checkout --orphan "$TARGET_BRANCH"
+  git checkout "$target_branch" || git checkout --orphan "$target_branch"
   git reset --hard
 }
 
-function repo_package_path() {
-  if [[ "$1" =~ ([^/]+)-([0-9.]+)(\.tar\.gz)$ ]]; then
-    echo "${BASH_REMATCH[1]}/${BASH_REMATCH[2]}/${BASH_REMATCH[1]}${BASH_REMATCH[3]}"
-  fi
+function copy_packages() {
+  function repo_package_path() {
+    if [[ "$1" =~ ([^/]+)-([0-9.]+)(\.tar\.gz)$ ]]; then
+      echo "${BASH_REMATCH[1]}/${BASH_REMATCH[2]}/${BASH_REMATCH[1]}${BASH_REMATCH[3]}"
+    fi
+  }
+
+  local src_path
+  for src_path in "$stack_dist_dir"/*-*.tar.gz; do
+    local dest_path="$(repo_package_path "$src_path")"
+    if [[ -z "$dest_path" ]]; then
+      continue
+    fi
+
+    mkdir -p "$(dirname "$dest_path")"
+    cp "$src_path" "$dest_path"
+  done
 }
 
-function copy_packages() {
-  for src_path in "$STACK_DIST_DIR/"*-*.tar.gz; do
-    local target_path="$(repo_package_path "$src_path")"
+function generate_index() {
+  local index_tarball="$1"
 
-    mkdir -p "$(dirname "$target_path")"
-    cp "$src_path" "$target_path"
+  local temp_dir="$(mktemp -d)"
+  local src_tarball
+  for src_tarball in */*/*.tar.gz; do
+    if [[ "$src_tarball" =~ ([^/]+/[0-9.]+)/([^/]+)\.tar\.gz ]]; then
+      local dest_dir="$temp_dir/${BASH_REMATCH[1]}"
+      local cabal_name="${BASH_REMATCH[2]}.cabal"
+    else
+      continue
+    fi
+
+    mkdir -p "$dest_dir"
+    tar zxOf "$src_tarball" "$cabal_name" > "$dest_dir/$cabal_name"
   done
+
+  tar zcf "$index_tarball" -C "$temp_dir" "$temp_dir"/*
 }
 
 setup_dir
 
 copy_packages
+generate_index '00-index.tar.gz'
+
 git add .
 
 if git diff --cached --quiet --exit-code; then
@@ -66,7 +92,7 @@ if git diff --cached --quiet --exit-code; then
 fi
 
 setup_git
-git commit -m "$GIT_COMMIT_MESSAGE"
+git commit -m "$git_commit_message"
 
 setup_ssh "$ENCRYPTION_LABEL"
-git push "$GIT_SSH_REPO" "$TARGET_BRANCH"
+git push "$git_ssh_repo" "$target_branch"
